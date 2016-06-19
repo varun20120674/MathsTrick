04fd94139278d73c758fb53138a3ccbbae0f239b
Varun Gupta <user@192.168.30.240>
2016-06-19 23:17:37 +0530

    Second Commit

---
+++ src/trick.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class trick {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("Think Any One Number In Your Mind");
+		 String a =scanner.nextLine();
+		 
+		 System.out.println("Multiply That Number With 3");
+		 String b =scanner.nextLine();
+		 
+		 System.out.println("Sum Up The Resulted Number with 45");
+		 String c =scanner.nextLine();
+		 
+		 System.out.println("Now... Double The Resulted number ");
+		 String d =scanner.nextLine();
+		 
+		 System.out.println("Now...Divide the number by 6");
+		 String f =scanner.nextLine();
+		 
+		 System.out.println("Remove the number that you take initially i.e. subtract that number from the newly resulted Number ");
+		 String g =scanner.nextLine();
+		 
+		 System.out.println("Machine is Calculating........Press Enter");
+		 String i =scanner.nextLine();
+	
+		 System.err.println("Answer is--->  15");
+		 
+
+
+	}
+
+}
+
+
+
